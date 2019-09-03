df287c739e2b9aabe9edf9c8e549a1481d8bc8d7
Ian Schwartz <ian.schwartz23@gmail.com>
2019-09-02 19:22:17 -0700

    DOM-I project MVP

--- js/index.js
+++ js/index.js
@@ -65,11 +65,44 @@ btnContent.textContent = siteContent["cta"] ["button"]
 const mainContent = document.querySelectorAll('.text-content h4');
 mainContent[0].textContent = siteContent["main-content"] ["features-h4"]
 mainContent[1].textContent = siteContent["main-content"] ["about-h4"]
+mainContent[2].textContent = siteContent["main-content"] ["services-h4"]
+mainContent[3].textContent = siteContent["main-content"] ["product-h4"]
+mainContent[4].textContent = siteContent["main-content"] ["vision-h4"]
 
 
 const mainContentP = document.querySelectorAll('.text-content p');
 mainContentP[0].textContent = siteContent["main-content"] ["features-content"]
 mainContentP[1].textContent = siteContent["main-content"] ["about-content"]
+mainContentP[2].textContent = siteContent["main-content"] ["services-content"]
+mainContentP[3].textContent = siteContent["main-content"] ["product-content"]
+mainContentP[4].textContent = siteContent["main-content"] ["vision-content"]
 
+const contactContent = document.querySelector('.contact h4');
+contactContent.textContent = siteContent["contact"]["contact-h4"]
+
+const contactContentP = document.querySelectorAll('.contact p');
+contactContentP[0].textContent = siteContent["contact"]["address"]
+contactContentP[1].textContent = siteContent["contact"]["phone"]
+contactContentP[2].textContent = siteContent["contact"]["email"]
+
+const footerContent = document.querySelector('footer p');
+footerContent.textContent = siteContent["footer"]["copyright"]
+
+// Task 3: Add new content
+const navColor = document.querySelectorAll('a');
+navColor[0].style.color = 'green'
+navColor[1].style.color = 'green'
+navColor[2].style.color = 'green'
+navColor[3].style.color = 'green'
+navColor[4].style.color = 'green'
+navColor[5].style.color = 'green'
+
+const newNavItem = document.querySelector('nav');
+const newNavText = document.createTextNode('History');
+newNavItem.appendChild(newNavText)
+
+const anotherNavItem = document.querySelector('nav');
+const anotherNavText = document.createTextNode('Team');
+anotherNavItem.prepend(anotherNavText)
 
 
